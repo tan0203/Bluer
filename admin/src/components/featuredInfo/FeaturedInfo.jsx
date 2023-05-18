9596b5ef3b5ee5f4c50525f759ed0775711d4c1c
LeTan <lengoctanw@gmail.com>
2023-05-18 10:11:21 +0700

    fixnew

--- admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,30 +1,26 @@
 import "./featuredInfo.css";
-import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
 
 export default function FeaturedInfo({ users, posts, comments }) {
-    return (
-        <div className="featured">
-            <div className="featuredItem">
-                <span className="featuredTitle">Users</span>
-                <div className="featuredMoneyContainer">
-                    <span className="featuredMoney">{users?.length}</span>
-                </div>
-                <span className="featuredSub">Compared to last month</span>
-            </div>
-            <div className="featuredItem">
-                <span className="featuredTitle">Likes</span>
-                <div className="featuredMoneyContainer">
-                    <span className="featuredMoney">{posts}</span>
-                </div>
-                <span className="featuredSub">Compared to last month</span>
-            </div>
-            <div className="featuredItem">
-                <span className="featuredTitle">Comments</span>
-                <div className="featuredMoneyContainer">
-                    <span className="featuredMoney">{comments}</span>
-                </div>
-                <span className="featuredSub">Compared to last month</span>
-            </div>
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <span className="featuredTitle">Users</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">{users?.length} </span>
         </div>
-    );
+      </div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Likes</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">{posts}</span>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Comments</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">{comments}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
